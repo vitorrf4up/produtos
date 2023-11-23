143a39b2ce36ff83b8c838234472e117150e1d21
Vitor <vitorrf4@gmail.com>
2023-11-23 12:01:13 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('loja', 'root', '***REMOVED***', {
+const sequelize = new Sequelize('loja', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
